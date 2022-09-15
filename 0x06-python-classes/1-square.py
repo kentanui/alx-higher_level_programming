91872a14512afb40eebc66e96f9471c121a85992
kentanui <kentanuironoh@gmail.com>
2022-09-14 18:10:06 -0700

    new files python exceptions

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+No module imported
+"""
+
+
+class Square:
+    """
+    Defines a square by private instance size,
+    instantiation with size.
+    """
+    def __init__(self, size):
+        """
+        size : no type/value verification
+        ----------------------------
+        Private instance attribute size
+        """
+        self.__size = size
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                                                                                    0,1           All
+

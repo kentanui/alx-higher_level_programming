c9b502f19c867500f0b534986f87df9afd95b5c2
kentanui <kentanuironoh@gmail.com>
2022-09-27 15:37:19 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""module contains function that writes to text file."""
+"""
+Contains the function "append_wrtie"
+"""
 
 
-def write_file(filename="", text=""):
-    """function writes a string to a text file (UTF8) and
-    returns the number of characters written
-    """
-    with open(filename, mode='w', encoding='UTF-8') as file:
-        nb_char = file.write(text)
-    return nb_char
+def append_write(filename="", text=""):
+    """returns the number of chars appended to "filename" from "text" """
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
